b3768d60f9cfbe5a8de73767dabb934d52ce9aa2
walterlee <walter.lee9@gmail.com>
2019-05-23 17:28:46 -0500

    scoping and ecmascript 2018 code

--- components/test.js
+++ components/test.js
@@ -1,13 +1,42 @@
-function findMissingNumber(arr) {
-    let add = (a, b) => a + b;
-    return arr.reduce(add);
+let a = 0;
+// var b = 0;
 
-    // for (var i = 1; i < arr.length; i++) {
-    //     if (arr[i] - arr[i - 1] != 1) {
-    //         //Not consecutive sequence, here you can break or do whatever you want
-    //         return arr[i] - 1;
-    //     }
-    // }
+function foo() {
+    var a = 1;
+    var b = 1;
+    console.log('foo: ' + a, b);
 }
 
-console.log(findMissingNumber([1, 2, 3, 4, 6, 7, 8, 9, 10]));
+function bar() {
+    console.log('bar: ' + a, b);
+}
+
+{
+    var b = 2;
+}
+
+console.log(a);
+console.log(b);
+
+foo();
+bar();
+
+var arr = [1, 2, 3, 4, 5];
+
+
+function moo() {
+    let abc = arr.map(x => [x, x * 2]);
+    return abc;
+}
+
+console.log(moo());
+
+var arrB = [1, 2, 3, [4, 5, 6, [7, 8, 9, [10, 11, 12]]]];
+
+function okay() {
+    let def = arrB.flat();
+    let bde = arrB.flat(Infinity);
+    return bde, def;
+}
+
+console.log(okay());
